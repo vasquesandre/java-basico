8711d03b15f46d7f85b11a68f6a758099cf1c46d
AndreVasques <133912523+vasquesandre@users.noreply.github.com>
2023-12-01 16:30:06 -0300

    update

---
+++ tipos-e-variaveis/src/SmartTv/SmartTv.java
@@ -0,0 +1,36 @@
+package SmartTv;
+
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar() {
+        ligada = true;
+    }
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Volume aumentado para: " + volume);
+    }
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Volume diminiuido para: " + volume);
+    }
+
+    public void aumentarCanal() {
+        canal++;
+        System.out.println("Canal " + canal);
+    }
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("Canal " + canal);
+    }
+    public void mudarCanal( int novoCanal ) {
+        canal = novoCanal;
+        System.out.println("Canal " + canal);
+    }
+}
---
+++ tipos-e-variaveis/src/SmartTv/Usuario.java
@@ -0,0 +1,29 @@
+package SmartTv;
+
+public class Usuario {
+    
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada: " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV Ligada: " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        smartTv.mudarCanal(15);
+
+        smartTv.desligar();
+        System.out.println("TV Ligada: " + smartTv.ligada);
+    }
+}
