518c68f8fbdd346215f51cf68e577c4cde6196c3
Andre Vasques <andrevasques@outlook.com.br>
2023-12-11 13:19:53 -0300

    update

--- Sintaxe/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Metodos"},{"p":"<Unnamed>","l":"Operadores"},{"p":"<Unnamed>","l":"TiposVariaveis"}];updateSearchResults();
--- Sintaxe/tipos-e-variaveis/src/docs/tag-search-index.js
+++
@@ -1,1 +0,0 @@
-tagSearchIndex = [{"l":"Calculadora","h":"class Calculadora","d":"Section","u":"Calculadora.html#calculadora-heading"}];updateSearchResults();
