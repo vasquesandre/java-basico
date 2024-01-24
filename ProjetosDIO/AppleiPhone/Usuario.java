a22ab3c4349a21c7bd37b2c2391889eb92d6af65
Andre Vasques <andrevasques@outlook.com.br>
2024-01-24 09:05:50 -0300

    update

--- ProjetosDIO/AppleiPhone/Usuario.java
+++ ProjetosDIO/AppleiPhone/Usuario.java
@@ -1,5 +1,27 @@
 package ProjetosDIO.AppleiPhone;
 
+import ProjetosDIO.AppleiPhone.AppleMusic.AppleMusic;
+import ProjetosDIO.AppleiPhone.Phone.Phone;
+import ProjetosDIO.AppleiPhone.Safari.Safari;
+import ProjetosDIO.AppleiPhone.iPhone.IPhone;
+
 public class Usuario {
-    
+    public static void main(String[] args) {
+        IPhone iphone = new IPhone();
+        Phone telefone = (Phone) iphone;
+        AppleMusic music = (AppleMusic) iphone;
+        Safari safari = (Safari) iphone;
+
+        telefone.ligar();
+        telefone.atender();
+        telefone.iniciarCorreioVoz();
+
+        music.reproduzir();
+        music.pausar();
+        music.selecionarMusica();
+
+        safari.adicionarNovaAba();
+        safari.atualizarPagina();
+        safari.exibirPagina();
+    }
 }
--- ProjetosDIO/AppleiPhone/iPhone/IPhone.java
+++ ProjetosDIO/AppleiPhone/iPhone/IPhone.java
@@ -5,5 +5,39 @@ import ProjetosDIO.AppleiPhone.Phone.Phone;
 import ProjetosDIO.AppleiPhone.Safari.Safari;
 
 public class IPhone implements AppleMusic, Phone, Safari {
-    
+    public void ligar() {
+        System.out.println("Ligando pelo iPhone");
+    }
+
+    public void exibirPagina() {
+        System.out.println("Exibindo página pelo iPhone");
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba pelo iphone");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página pelo iphone");
+    }
+
+    public void atender() {
+        System.out.println("Atendendo telefone pelo iphone");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz pelo iphone");
+    }
+
+    public void reproduzir() {
+        System.out.println("Reproduzindo música pelo iphone");
+    }
+
+    public void pausar() {
+        System.out.println("Pausando música pelo iphone");
+    }
+
+    public void selecionarMusica() {
+        System.out.println("Selecionando música pelo iphone");
+    }
 }
