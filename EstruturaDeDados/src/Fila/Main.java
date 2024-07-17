38464f95956eb1a85ddafff11764709d06b184b7
Andre Vasques <andrevasques@outlook.com.br>
2024-07-17 14:49:21 -0300

    update

---
+++ EstruturaDeDados/src/Fila/Main.java
@@ -0,0 +1,28 @@
+package Fila;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Filas<String> minhaFila = new Fila<>();
+
+        minhaFila.enqueue("primeiro");
+        minhaFila.enqueue("segundo");
+        minhaFila.enqueue("terceiro");
+        minhaFila.enqueue("quarto");
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila);
+
+        minhaFila.enqueue("ultimo");
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.first());
+
+    }
+
+}
---
+++ EstruturaDeDados/src/ListaEncadeada/Main.java
@@ -0,0 +1,22 @@
+package ListaEncadeada;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada<String>();
+
+        minhaListaEncadeada.add("teste1");
+        minhaListaEncadeada.add("teste2");
+        minhaListaEncadeada.add("teste3");
+        minhaListaEncadeada.add("teste4");
+
+        System.out.println(minhaListaEncadeada);
+
+        System.out.println(minhaListaEncadeada.remove(3));
+
+        System.out.println(minhaListaEncadeada);
+
+    }
+
+}
